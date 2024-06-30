169c6bc6177143432461a241fabb8781a8339d81
Pokruk <46229617+Pokruk@users.noreply.github.com>
2024-06-30 07:54:46 +0300

    Add project files.

---
+++ SecondLookSkeletonFix/Patches/GhostNotLoadedFix.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace SecondLookSkeletonFix.Patches {
+    [HarmonyPatch(typeof(SecondLookSkeleton))]
+    [HarmonyPatch("SetTappedState", MethodType.Normal)]
+    internal class GhostNotLoadedFix {
+        public static bool Prefix(SecondLookSkeleton __instance) {
+            if (__instance.spookyText == null) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ SecondLookSkeletonFix/PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace SecondLookSkeletonFix {
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo {
+        public const string GUID = "com.pokruk.gorillatag.secondlookskeletonfix";
+        public const string Name = "SecondLookSkeletonFix";
+        public const string Version = "1.0.0";
+    }
+}
